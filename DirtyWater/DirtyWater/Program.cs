6d3d2b82523456a88e6ef566d336b975c5524c8d
Nikoraito <nikoraito@gmail.com>
2015-11-16 05:24:47 -0500

    got dang

---
+++ DirtyWater/DirtyWater/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirtyWater
+{
+    class Program
+    {
+        private static byte[] _buffer = new byte[1024];
+        private static List<Socket> _clientSockets = new List<Socket>();
+        private static Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            SetupServer();
+            Console.ReadLine();
+        }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("Setting up server...");
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, 100));
+            _serverSocket.Listen(5);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket = _serverSocket.EndAccept(AR);
+            _clientSockets.Add(socket);
+            Console.WriteLine("Client Connected");
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            Console.WriteLine("Waiting on Client...");
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Console.WriteLine("Data Recieved from Client");
+            Socket socket = (Socket)AR.AsyncState;
+            int received = socket.EndReceive(AR);
+            byte[] dataBuf = new byte[received];
+            Console.WriteLine("Processing data...");
+            Array.Copy(_buffer, dataBuf, received);
+
+            string text = Encoding.ASCII.GetString(dataBuf);
+            Console.WriteLine("Text received: " + text);
+
+            //string response = string.Empty;
+
+            byte[] data;
+
+            if (text[0] == '@')
+            {
+                data = Encoding.ASCII.GetBytes(ParseInput(text));
+            }
+            else {
+                data = Encoding.ASCII.GetBytes(text);
+            }
+
+            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+        }
+
+        private static void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+
+
+        private static String ParseInput(String s) {
+            String report = "";
+            String name = "";
+            String pass = "";
+
+            int i = 1;
+            switch(s[i++]){
+                case 'L':
+                    report += "Login initiated, ";
+                    break;
+                case 'N':
+                    report += "New account created, ";
+                    break;
+                case 'U':
+                    report += "Account Unregistered, ";
+                    break;
+                default:
+                    report += "Undefined";
+                    break;                    
+            }
+
+            if (s[i] == '/') {
+                while(s[++i] != '/')
+                {
+                    name += s[i];
+                }
+                while (s[++i] != '?') // ADD A THING FOR SAFETY
+                {
+                    pass += s[i];
+                }
+            }
+
+            report += "\nN: " + name + "\nP: " + pass;
+            Console.WriteLine(report);
+
+            if (pass == "Hella")
+            {
+                return "Affirmative";
+            }
+            else {
+                return "Negative";
+            }
+
+        }
+
+
+    }
+}
---
+++ DirtyWaterClient/DirtyWaterClient/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirtyWaterClient
+{
+    class Program
+    {
+        private static Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            LoopConnect();
+            SendLoop();
+            Console.ReadLine();
+        }
+
+        private static void SendLoop()
+        {
+            while (true)
+            {
+                Console.Write("Enter a request: ");
+                string req = Console.ReadLine();
+                byte[] buffer = Encoding.ASCII.GetBytes(req);
+                Console.WriteLine("Request Sending...");
+                _clientSocket.Send(buffer);
+                Console.WriteLine("Request Sent");
+
+                byte[] receivedBuf = new byte[1024];
+                int rec = _clientSocket.Receive(receivedBuf);
+                byte[] data = new byte[rec];
+                Array.Copy(receivedBuf, data, rec);
+                Console.WriteLine("Received: " + Encoding.ASCII.GetString(data));
+            }
+        }
+
+        private static void LoopConnect()
+        {
+            int attempts = 0;
+
+            IPAddress[] IPs = Dns.GetHostAddresses("107.191.103.148");
+
+            while (!_clientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    _clientSocket.Connect(IPs[0], 100);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Connection attempts: " + attempts.ToString());
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Connected");
+        }
+    }
+}
