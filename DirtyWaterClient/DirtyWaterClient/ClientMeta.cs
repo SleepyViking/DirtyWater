8e46ab240073908ab31dd5fcdb60c6a63ec27633
Nikoraito <nikoraito@gmail.com>
2015-11-22 14:54:19 -0500

    Added a bunch of shit again ugh
    Added two Client classes:
    - ClientMeta.cs - called to craft and send login packets to the server.
    - MetaMenu.cs - a command line wrapper for ClientMeta that accepts some
    text and transforms them into ClientMeta calls, and therefor requests.

---
+++ DirtyWaterClient/DirtyWaterClient/ClientMeta.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirtyWaterClient
+{
+    class ClientMeta
+    {
+
+        public static string request = "";
+
+        public static void Login(string username, SecureString password) {
+            if (Validate(username, password) == 0)
+            {
+                request = "@L\x00" + username.PadRight(16, '\0') + PassHash(password).PadRight(16, '\0');  
+
+            }
+        }
+
+        public static void Logout(string username) {
+            if (Validate(username) == 0)
+            {
+                request = "@LO";
+            }
+            
+
+        }
+
+        public static void Register(string username, SecureString password, string email) {
+            if (Validate(username, password) == 0)
+            {
+                request = "@R\x00";
+            }
+        }
+
+        public static void Unregister(string username, SecureString password) {
+            if (Validate(username, password) == 0)
+            {
+                request = "@U\x00";
+            }
+        }
+
+        public static byte Validate(string username, SecureString password) {
+            //salt and hash the password HERE before sending it along to the 
+            //string assembly methods
+            return 0;
+        }
+
+        public static byte Validate(string username)
+        {
+            //Check to see if Username exists, and if it does, if the password matches.
+            //done before any other processing.
+            //Should check their flags to see if they are banned, temporarily or permanently.
+
+            return 0;
+        }
+
+        public static string PassHash(SecureString s) {
+
+
+            s.Dispose();
+            return "";
+        }
+
+        public static byte[] Serialize(string input) {
+            byte[] bytes = new byte[128];
+            return bytes;
+        }
+
+
+    }
+}
---
+++ DirtyWaterClient/DirtyWaterClient/MetaMenu.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirtyWaterClient
+{
+    class MetaMenu
+    {
+
+        string user;
+
+        public MetaMenu(){
+
+            user = "";
+
+        }
+
+        private void Start() {
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine(
+                    "Welcome to the DirtyWater sub-prealpha menu.\n" +
+                    "Please type one of the commands below to begin.\n" +
+                    " * register\n" +
+                    " * login"
+                );
+
+                Prompt(user);
+            }
+        }
+
+        private void Prompt(string user)
+        {
+            bool valid = false;
+            string input;
+
+            while (!valid)
+            {
+                Console.WriteLine("{0}> ", user);
+                input = Console.ReadLine();
+                SecureString pass;
+
+                switch (input)
+                {
+
+                    
+                    case "login":
+                        Console.WriteLine("Username: ");
+                        user = Console.ReadLine();
+                        Console.WriteLine("Password: ");
+                        pass = GetPassword();
+                        ClientMeta.Login(user, pass);
+                        break;
+
+                    case "register":
+                        string email = "";
+                        while (true) {
+                            Console.WriteLine("Please enter your Email address: ");
+                            email = Console.ReadLine();
+                            Console.WriteLine("Create a username: ");
+                            user = Console.ReadLine();
+                            Console.WriteLine("Create a password: ");
+                            pass = GetPassword();
+                            Console.WriteLine("Repeat Password: ");
+                            if (GetPassword() == pass) break;
+                        }
+                        ClientMeta.Register(user, pass, email);
+                        break;
+
+                    default:
+                        Console.WriteLine("Sorry, that command is not recognized.");
+                        break;
+                }
+            }
+
+        }
+
+        private static SecureString GetPassword() {
+            SecureString password = new SecureString();
+            while (true)
+            {
+                ConsoleKeyInfo i = Console.ReadKey(true);
+                if (i.Key == ConsoleKey.Enter) break;
+                else if (i.Key == ConsoleKey.Backspace)
+                {
+                    if (password.Length > 0)
+                    {
+                        password.RemoveAt(password.Length - 1);
+                        Console.Write("\b \b");
+                    }
+                }
+                else {
+                    password.AppendChar(i.KeyChar);
+                    Console.Write("*");
+                }
+
+            }
+            return password;
+        }
+
+
+    }
+}
